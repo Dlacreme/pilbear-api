5d0e34af9711ec5ea3c5fbee4af62a1febf18533
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2020-03-07 15:16:29 +0900

    add upload meta data

--- src/services/s3.cr
+++ src/services/s3.cr
@@ -6,8 +6,22 @@ module Pilbear::Services
     @@client = Awscr::S3::Client.new("eu-central-1", "AKIAXPNROKWRXDPW2OCV", "MNEMmweFRlsRuwMd7vIeTdiVhwFwTm7nvWJfK4D7")
 
     def self.put(file : File, name : String) : String
-      @@client.put_object(@@bucket_name, name, file)
+      @@client.put_object(@@bucket_name, name, file,
+        {"x-amz-acl" => "public-read", "Content-Type" => self.get_content_type(File.extname(name))}
+      )
       "https://pilbeardev.s3.eu-central-1.amazonaws.com/#{name}"
+    end
+
+    private def self.get_content_type(extension : String) : String
+      case extension
+      when ".jpg"
+        return "image/jpeg"
+      when ".jpeg"
+        return "image/jpeg"
+      when ".png"
+        return "image/png"
+      end
+      return ""
     end
   end
 end
